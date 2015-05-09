62f73273c9cfb6e49be2eaca4bac64c080d42939
Santiago Passalacqua <passalacquasantiago@gmail.com>
2015-05-09 20:44:26 -0300

    agregue metodos a tablero

--- src/main/java/com/unrc/app/Tablero.java
+++ src/main/java/com/unrc/app/Tablero.java
@@ -6,14 +6,24 @@ package com.unrc.app;
  * @author santiago
  */
 public class Tablero {
-    private final int [][]matriz;
+    private  int [][]matriz;
     
     public Tablero(int filas,int columnas){
         matriz=new int[filas][columnas];
+        for(int i=0;i<filas;i++){
+             for(int j=0;j<columnas;j++){
+                 matriz[i][j]=0;
+            }
+        }
     }
     //constructor por defecto
     public Tablero(){
         matriz=new int[7][7];
+        for(int i=0;i<7;i++){
+             for(int j=0;j<7;j++){
+                 matriz[i][j]=0;
+            }
+        }
     }
 
     public int[][] getMatriz() {
@@ -21,6 +31,9 @@ public class Tablero {
     }  
     public void setCasilla(int fila,int columna,int valor){
         matriz[fila][columna]=valor;
+    }
+    public int getCasilla(int fila,int columna){
+        return matriz[fila][columna];
     }
     
 }

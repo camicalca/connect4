e0e1debd6be69ad3649133c8b14fb1c87a67bf1e
agustin <agustinmartinez01@gmail.com>
2015-06-12 16:48:22 -0300

    subo imagen122

--- src/main/java/com/unrc/app/Board.java
+++ src/main/java/com/unrc/app/Board.java
@@ -46,16 +46,74 @@ public class Board {
     public int getCell(int row,int column){
         return matrix[row][column];
     }
+    
+    private String returnImage(int number){
+        if (number==0){
+            return "<img src='http://k30.kn3.net/E/3/4/5/3/C/4B3.png'>";
+        }else if(number==1){
+            return "<img src='http://k33.kn3.net/6/4/0/8/A/1/EBB.png'>";
+        
+        }else if (number==2){
+            return "<img src='http://k33.kn3.net/B/C/D/2/E/D/7C9.png'>";
+            
+        }else{
+            return null;
+        }
+    
+    
+    }
+    
+    
     public String toStringB(){
      String tabla;
-            tabla="<tr>\n" +		
-                "<td>"+this.getCell(0,0)+"</td>\n" +
- "<td>"+this.getCell(0,1)+"</td>\n"+
-"<td>"+this.getCell(0,2)+"</td>\n" +
-"<td>"+this.getCell(0,3)+"</td>\n" +
-"<td>"+this.getCell(0,4)+"</td>\n" +
-"<td>"+this.getCell(0,5)+"</td>\n" +
-"		</tr>";
+            tabla="<table><tr>\n" +		
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,1))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,2))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,3))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,4))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,5))+"</td>\n" +
+                "</tr>\n"+
+                
+                "<tr>\n" +		
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n"+
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "</tr>\n"+
+                "<tr>\n" +		
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n"+
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "</tr>\n"+
+                
+                "<tr>\n" +		
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n"+
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "</tr>\n"+
+                "<tr>\n" +		
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n"+
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "<td>"+returnImage(getCell(0,0))+"</td>\n" +
+                "</tr>\n</table>"
+                    
+                    
+                    
+                    
+                    
+                    ;
                     
                    
             return tabla;
